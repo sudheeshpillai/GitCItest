acd4452408c72292c3be347e13f672b5df7a0dac
sudheeshpillai <sudheesh.sukumarapillai@cognizant.com>
2017-03-30 18:22:07 +0530

    test modified

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 var request = require("supertest");
-//var should = require("chai").should();
-//var expect = require('chai').expect;
+var should = require("chai").should();
+var expect = require('chai').expect;
 
 // This agent refers to PORT where program is runninng.
 
-//var server = supertest.agent("https://www.cognizant.com");
-var app = require("../app").getApp;
+var server = request.agent("https://www.cognizant.com");
+//var app = require("../app").getApp;
 
 // UNIT test begin
 
@@ -16,17 +16,17 @@ describe("SAMPLE unit test",function(){
   it("should return home page",function(done){
 
     // calling home page api
-    request(app)
+    server
     .get("/")
     //.expect("Content-type",/json/)
-    .expect(200, done) // THis is HTTP response
-    //.end(function(err,res){
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
       // HTTP status should be 200
-      //expect(res.status).to.have.equal(200);
+      expect(res.status).to.have.equal(200);
       // Error key should be false.
       //res.body.error.should.equal(false);
-      //done();
-    //});
+      done();
+    });
   });
 
 });
