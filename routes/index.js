dc8d63d504047768eb68b335ba5409ddce49c995
sudheeshpillai <sudheesh.sukumarapillai@cognizant.com>
2017-04-04 20:12:54 +0530

    files updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My First Node App' });
+  res.render('index', { title: 'The node App' });
 });
 
 module.exports = router;
